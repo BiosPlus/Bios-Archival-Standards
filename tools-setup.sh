a1b2e5296edd677719beff7c12be96a6509ec196
Michael Jon <9003059+BiosPlus@users.noreply.github.com>
2019-01-29 23:32:35 +1100

    Create tools-setup.sh

---
+++ tools-setup.sh
@@ -0,0 +1,5 @@
+#!usr/bin/env bash
+#The tools I commonly use and a quick installer to grab them all in bulk.
+#To run this execute the following: curl https://raw.githubusercontent.com/BiosPlus/Bios-Archival-Standards/master/tools-setup.sh | sudo bash
+
+sudo apt-get install wget2 screen fuse htop python3 && sudo pip install youtube-dl
